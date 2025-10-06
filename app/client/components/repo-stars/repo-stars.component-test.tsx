a2cbf4a27c3f628a8a3c0312dda88c47a6b6d664
Rui Ramos <rui@goodfit.io>
2025-10-06 23:05:10 +0100

    More component tests

--- app/client/components/repo-stars/repo-stars.component-test.tsx
+++ app/client/components/repo-stars/repo-stars.component-test.tsx
@@ -1,5 +1,6 @@
 import { test, expect } from "@playwright/experimental-ct-react";
 import RepoStars from "./repo-stars";
+import { useState } from "react";
 
 test("should render the component with example data", async ({ mount }) => {
   const repoName = "facebook/react";
@@ -19,4 +20,42 @@ test("should render the component with example data", async ({ mount }) => {
     />,
   );
   await expect(component).toContainText(`Stars on ${repoName}: ✨ ${stars} ✨`);
+});
+
+test("button is disabled when input is empty", async ({ mount }) => {
+  const repoName = "";
+
+  const component = await mount(
+    <RepoStars
+      repoName={repoName}
+      setRepoName={() => {}}
+      data={{ stars: 0 }}
+      isLoading={false}
+      isRefetching={false}
+      isSuccess={true}
+      isError={false}
+      refetch={() => {}}
+      error={null}
+    />,
+  );
+  await expect(component.locator("button")).toBeDisabled();
+});
+
+test("shows loading message while data is loading", async ({ mount }) => {
+  const repoName = "example";
+
+  const component = await mount(
+    <RepoStars
+      repoName={repoName}
+      setRepoName={() => {}}
+      data={{ stars: 0 }}
+      isLoading={true}
+      isRefetching={true}
+      isSuccess={true}
+      isError={false}
+      refetch={() => {}}
+      error={null}
+    />,
+  );
+  await expect(component).toContainText(`Loading`);
 });
